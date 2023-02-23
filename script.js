92157b55a2caaddca5890bc37c9271a355e004fa
Stevennius Chandra <stevenniuschandra@gmail.com>
2023-02-23 21:17:03 +0700

    grammar correction

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 const tMatch = document.querySelector('#tMatch'),
-tWr = document.querySelector('#tWr'),
-wrReq = document.querySelector('#wrReq'),
-btn = document.querySelector('.btn-submit'),
-result = document.querySelector('.result-place p'),
-sosmed = document.querySelectorAll('.img');
+  tWr = document.querySelector('#tWr'),
+  wrReq = document.querySelector('#wrReq'),
+  btn = document.querySelector('.btn-submit'),
+  result = document.querySelector('.result-place p'),
+  sosmed = document.querySelectorAll('.img');
 
 btn.addEventListener('click', () => {
-  if(tMatch.value && tWr.value && wrReq.value !== ''){
-    
+  if (tMatch.value && tWr.value && wrReq.value !== '') {
+
     const values = {
       tMatch: parseInt(tMatch.value),
       tWr: parseInt(tWr.value),
       wrReq: parseInt(wrReq.value)
     };
-    
-    if(values.tMatch && values.tWr && values.wrReq !== NaN){
-      
-      if(values.tWr < values.wrReq){
-        if(values.tWr && values.wrReq <= 99){
-        const hasil = rumus(values.tMatch, values.tWr, values.wrReq);
-        
-        result.innerHTML = `Kamu harus Memainkan <span class="bold">${hasil}</span> Match tanpa Kalah untuk Mendapatkan <span class="bold">${values.wrReq}%</span> Winrate`;
-        }else{
-          result.innerHTML = `WR lu <span class="bold">100%</span> ? Halah...`;
+
+    if (values.tMatch && values.tWr && values.wrReq !== NaN) {
+
+      if (values.tWr < values.wrReq) {
+        if (values.tWr && values.wrReq <= 99) {
+          const hasil = rumus(values.tMatch, values.tWr, values.wrReq);
+
+          result.innerHTML = `Kamu harus Memainkan <span class="bold">${hasil}</span> Match tanpa Kalah untuk Mendapatkan <span class="bold">${values.wrReq}%</span> Winrate`;
+        } else {
+          result.innerHTML = `WR <span class="bold">100%</span> GA MUNGKIN BROO`;
         }
-      }else{
+      } else {
         result.innerHTML = `<sapn class="red">Unknown Error!</span>`;
       }
     }
-  }else{
+  } else {
     result.innerHTML = `<sapn class="red">Unknown Error!</span>`;
   }
 });
 
 sosmed.forEach(el => {
   el.addEventListener('click', () => {
-  if(el.classList.contains('github-link')){
-    window.location.href = 'https://github.com/SCxoneZ'
-  }else if(el.classList.contains('tiktok-link')){
-    window.location.href = 'https://vt.tiktok.com/ZSeuGDS5F/';
-  }else if(el.classList.contains('instagram-link')){
-    window.location.href = 'https://instagram.com/int_to_str?utm_medium=copy_link';
-  }
+    if (el.classList.contains('github-link')) {
+      window.location.href = 'https://github.com/SCxoneZ'
+    } else if (el.classList.contains('tiktok-link')) {
+      window.location.href = 'https://vt.tiktok.com/ZSeuGDS5F/';
+    } else if (el.classList.contains('instagram-link')) {
+      window.location.href = 'https://instagram.com/stevennius_chandra';
+    }
   });
 });
 
 
 // function taken from johsteven.github.io/penghitung-wr
 function rumus(tMatch, tWr, wrReq) {
-    let tWin = tMatch * (tWr / 100);
-    let tLose = tMatch - tWin;
-    let sisaWr = 100 - wrReq;
-    let wrResult = 100 / sisaWr;
-    let seratusPersen = tLose * wrResult;
-    let final = seratusPersen - tMatch;
-    return Math.round(final);
+  let tWin = tMatch * (tWr / 100);
+  let tLose = tMatch - tWin;
+  let sisaWr = 100 - wrReq;
+  let wrResult = 100 / sisaWr;
+  let seratusPersen = tLose * wrResult;
+  let final = seratusPersen - tMatch;
+  return Math.round(final);
 }
